910935cc6f6e93fc9b30b9bcd624ec35a50d0641
kian_nrt7 <kiannosratian@gmail.com>
2024-04-23 14:06:04 +0330

    link list algoritm

--- main.c
+++ main.c
@@ -6,16 +6,58 @@
 
 
 
-
-
-
 int main (void) {
-
-
-
+   
 
     return 0;
 }
+
+// link list
+
+typedef struct Node{
+    int data ;
+    struct Node* next;
+}Node;
+
+
+Node* createNode(int data){
+    Node* newNode= (Node*)malloc(sizeof(Node));
+    if(!newNode){
+        printf("memory aloocation faild!\n");
+        exit(1);
+    }
+    newNode ->data = data;
+    newNode ->next = NULL;
+    return newNode;
+}
+
+void printList(Node* node){
+    while(node){
+        printf("%d -> ", node->data);
+        node = node->next;
+    }
+    printf("null\n");
+}
+
+ // Node* node1 = createNode(3);
+    // Node* node2 = createNode(4);
+    // Node* node3 = createNode(5);
+    // Node* node4 = createNode(6);
+
+    // node1->next = node2;
+    // node2->next = node3;
+    // node3->next = node4;
+    
+
+    // printList(node1);
+
+    // free(node1);
+    // free(node2);
+    // free(node3);
+    // free(node4);
+
+
+
 
 
 
