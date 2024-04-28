862132a363ed47697e44219af12c6dfae15f7e52
kian_nrt7 <kiannosratian@gmail.com>
2024-04-28 22:28:41 +0330

    stack algorithm is done

--- arrayAlg.c
+++ arrayAlg.c
@@ -1,4 +1,88 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+
+int main(void){    
+    
+   
+
+    return 0;
+}
+
+/////////////stack
+typedef struct Stack{
+    int *stack;
+    int top;
+    int capacity;
+}Stack;
+
+Stack* createStack(int capa){
+    Stack* newStack = malloc(sizeof(Stack));
+    newStack->stack = (size_t*) malloc(capa * sizeof(size_t));
+    newStack->top = -1;
+    newStack->capacity = capa;
+    return newStack;
+}
+/// push element
+void push( Stack *s , int element ){
+    if(s->top == s->capacity - 1){
+        printf("stack is full\n");
+        return;
+    }
+    s->stack[++s->top] = element;
+   // printf("top:%d\ncapacity:%d\n",s->top,s->capacity);
+    // printf("top:%d\n",++s->top);
+}
+
+
+// pop element 
+int pop (Stack *s){
+    if(s->top == -1){
+        printf("Stack is empty\n");
+        return -1 ;
+    }
+    return s->stack[s->top--];
+}
+
+// peakElement
+int peek(Stack *s){
+    if(s->top == -1){
+        printf("stack is empty");
+        return -1;
+    }
+    return s->stack[s->top];
+}
+
+// isEmpty
+
+int isEmpty(Stack *s){
+    return s->top == -1;
+}
+
+int size(Stack *s){
+    return s->top + 1;
+}
+
+
+
+//print stack
+void printStack(Stack *s){
+    for(int i = 0 ;i <= s->top ; ++i){
+        printf("%c\n",s->stack[i]);
+    }
+}
+
+
+
+
+
+
+
+
+
 
 // search/insert and delet element from unsorted array;
 //search 
@@ -13,8 +97,8 @@ int findNumber(int arr[],int n , int key){
     }
         return -1;
 }
-//insert function
 
+//insert function
 int  insertion(int arr[],int n , int insertNumber,int capacity){
     
     if(n >= capacity){
@@ -48,8 +132,13 @@ int find (int arr[],int n , int key){
         if(arr[i] == key)return i;
     }
     return -1;
-
 }
+
+
+
+
+
+
 int  deletElement (int arr[],int n ,int deletNumber){
     int pos = find(arr,n,deletNumber);
     int i ;
@@ -61,10 +150,3 @@ int  deletElement (int arr[],int n ,int deletNumber){
 
 }
 
-
-int main(void){    
-    
-
-
-    return 0;
-}
