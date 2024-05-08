ee627801bb53b45345e23059e9bc6a0cf39c1f66
kian_nrt7 <kiannosratian@gmail.com>
2024-05-08 14:14:47 +0330

    start treenode data structur

--- trees.c
+++ trees.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
 ///trees in algorithm 
+typedef struct  TreeNode {
+    char data;
+    struct TreeNode* left;
+    struct TreeNode* right;
+}TreeNode;
+
+
+
+
+
 
 
 int main(void){
