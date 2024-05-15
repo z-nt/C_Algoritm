363532601d78fcefded2fe8baadd18816456a11d
kian_nrt7 <kiannosratian@gmail.com>
2024-05-15 19:33:35 +0330

    dsa graphs implementation

--- graphs.c
+++ graphs.c
@@ -1,11 +1,51 @@
 #include <stdio.h>
 
-int main(void){
-    puts("hello graphs");
+void printgraphs(int matrix[4][4],int size){
+    for(int i = 0 ; i < size ; i++){
+            for(int j = 0 ; j < size ; j++){
+                printf("%d",matrix[i][j]);
+            }
+            printf("\n");
+    }
+}
 
+void printContection(int matrix[4][4] , char vertexdata[4] , int size){
+
+    for(int i = 0 ; i < size ; i++){
+        printf("%c: ",vertexdata[i]);
+        for(int j = 0 ; j < size ; j++){
+            if (matrix[i][j])
+            {
+                printf("%c  " ,vertexdata[j]);
+            }
+            
+        }
+        printf("\n");
+    }
+
+
+
+
+}
+
+
+
+int main(void){
+    char vertexData[4] = {'A','B','C','D'};
+    int matrix[4][4] = {
+        {1,1,1,1},
+        {1,1,1,0},
+        {1,1,0,0},
+        {1,0,1,1}
+    };
+
+    printgraphs(matrix,4);
+    printContection(matrix,vertexData,4);
 
     return 0;
 }
+
+
 
 
 
